2dacb196a0c873819a8c1654a8aa6dc4d71ca368
Gabriel Nogueira <gabrieln500@gmail.com>
2022-07-14 09:42:19 -0300

    second commit

--- js/form.js
+++ js/form.js
@@ -1,27 +1,29 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
+botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
+
     var paciente = obtemPacienteDoFormulario(form);
 
     var erros = validaPaciente(paciente);
 
-    if(erros.length > 0){
+    if (erros.length > 0) {
         exibeMensagensDeErro(erros);
+
         return;
     }
 
-    var pacienteTr = montaTr(paciente);
+    adicionaPacienteNaTabela(paciente);
 
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
     form.reset();
 
-    var mensagemErro = document.querySelector("#mensagem-erro");
-    mensagemErro.innerHTML = "";
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
-function obtemPacienteDoFormulario(form){
+});
+
+function obtemPacienteDoFormulario(form) {
 
     var paciente = {
         nome: form.nome.value,
@@ -34,21 +36,9 @@ function obtemPacienteDoFormulario(form){
     return paciente;
 }
 
-function exibeMensagensDeErro(erros){
-    var ul = document.querySelector("#mensagem-erro");
-    ul.innerHTML = "";
-
-
-    erros.forEach(function(erro){
-        var li = document.createElement("li");
-        li.textContent = erro;
-        ul.appendChild(li);
-    });
-}
-
-function montaTr(paciente){
+function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente")
+    pacienteTr.classList.add("paciente");
 
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
     pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
@@ -59,42 +49,58 @@ function montaTr(paciente){
     return pacienteTr;
 }
 
-function montaTd(dado,classe){
-    var td =  document.createElement("td");
-    td.textContent = dado;
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
     td.classList.add(classe);
+    td.textContent = dado;
 
     return td;
 }
 
-function validaPaciente(paciente){
+function validaPaciente(paciente) {
 
     var erros = [];
 
-    if(paciente.nome.length == 0){
-        erros.push("O nome não pode ser em branco")
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
     }
 
-    if(!validaPeso(paciente.peso)){
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
         erros.push("Peso é inválido");
     }
 
-    if(!validaAltura(paciente.altura)){
-        erros.push("Altura é inválida");   
-    }
-
-    if(paciente.gordura.length == 0){
-        erros.push("A gordura não pode ser em branco")
-    }
-
-    if(paciente.peso.length == 0){
-        erros.push("O peso não pode ser em branco")
-    }
-
-    if(paciente.altura.length == 0){
-        erros.push("A altura não pode ser em branco")
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
     }
 
     return erros;
 }
-})
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,13 +1,13 @@
-var pacientes = document.querySelectorAll(".paciente")
+var pacientes = document.querySelectorAll(".paciente");
 
-var tabela = document.querySelector("table");
+var tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("dblclick",function(event){
-    event.target.parentNode.classList.add("fadeOut")
-    setTimeout(function(){
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function() {
         event.target.parentNode.remove();
-    },500);
+    }, 500);
+
 });
-
-
 
